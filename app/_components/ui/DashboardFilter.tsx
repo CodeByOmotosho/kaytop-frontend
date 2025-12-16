595179c341c3e6cd4bb373c0a1432855b05c2079
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-16 19:36:30 +0100

    fix(ui): add custom value to dashboard filter

--- app/_components/ui/DashboardFilter.tsx
+++ app/_components/ui/DashboardFilter.tsx
@@ -6,7 +6,7 @@ export default function DashboardFilter(): JSX.Element {
     <Filter
       filterField="last"
       options={[
-        { value: "last_12_months", label: "12 months" },
+        { value: "custom", label: "12 months" },
         { value: "last_30_days", label: "30 days" },
         { value: "last_7_days", label: "7 days" },
         { value: "last_24_hours", label: "24 hours" },
