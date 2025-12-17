e70e3bcfc4f5724bee4b25f02f397669d65e8563
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-17 17:27:56 +0100

    feat(service): add dashboard service

--- app/services/dashboardService.ts
+++ app/services/dashboardService.ts
@@ -2,14 +2,20 @@ import apiClient from "@/lib/apiClient";
 import { apiBaseUrl } from "@/lib/config";
 import { AxiosError } from "axios";
 
+interface DashboardProps {
+  timeFilter: string;
+  startDate?: string | null;
+  endDate?: string | null;
+}
+
 export class DashboardService {
-  static async getDashboardKpi(timeFilter: string) {
-   
+  static async getDashboardKpi({ timeFilter, startDate, endDate }: DashboardProps) {
     try {
-      
       const response = await apiClient.get(`${apiBaseUrl}/dashboard/kpi`, {
         params: {
           timeFilter,
+          startDate, 
+          endDate
         },
       });
       console.log(response);
