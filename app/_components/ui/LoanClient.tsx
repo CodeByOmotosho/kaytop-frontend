e4d2c530395cd2b8bbc695c455ecdea004920b61
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-02 11:46:25 +0100

    refactor(ui): refactor loan client component

--- app/_components/ui/LoanClient.tsx
+++ app/_components/ui/LoanClient.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useDashboardQuery } from "@/app/dashboard/bm/queries/kpi/useDashboardQuery";
 import { useBranchLoans } from "@/app/dashboard/bm/queries/loan/useBranchLoans";
+import { useLoanDetails } from "@/app/dashboard/bm/queries/loan/useLoanDetails";
 import { usePageChange } from "@/app/hooks/usePageChange";
 import { PaginationKey } from "@/app/types/dashboard";
 import { getBranchLoanMetrics } from "@/lib/utils";
 import DashboardHeader from "./DashboardHeader";
-import DrawerTable from "./DrawerTable";
+import LoanSummary from "./LoanSummary";
 import Metric from "./Metric";
 import BranchLoanTable from "./table/BranchLoanTable";
+import LoanRepaymentTable from "./table/LoanRepaymentTable";
 
 export default function LoanClient() {
   const { isLoading, error, data } = useDashboardQuery();
@@ -19,8 +21,17 @@ export default function LoanClient() {
     data: branchLoan,
   } = useBranchLoans();
 
-  
-    const { handlePageChange } = usePageChange();
+  const { handlePageChange, setContextParam } = usePageChange();
+
+  function handleClick(loanId: number) {
+    setContextParam(loanId, PaginationKey.loan_page_id);
+  }
+
+  const {
+    isLoading: isLoadingDetails,
+    error: loanDetailsError,
+    data: loanDetails,
+  } = useLoanDetails();
 
   return (
     <>
@@ -31,12 +42,12 @@ export default function LoanClient() {
       <div className=" drawer drawer-end">
         <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
-          <label
+          {/* <label
             htmlFor="my-drawer-5"
             className="underline cursor-pointer drawer-button text-md decoration-brand-purple text-brand-purple"
           >
             View Loan
-          </label>
+          </label> */}
         </div>
         <div className="z-40 drawer-side ">
           <label
@@ -48,59 +59,27 @@ export default function LoanClient() {
             <h1 className="font-semibold text-center text-md text-neutral-700">
               Loan Details
             </h1>
-            <div className="flex justify-between px-5 py-2">
-              <div>
-                <p className="text-sm font-semibold text-gray-500">Loan ID</p>
-                <h1 className="text-sm text-neutral-700">BN-B1E73DA–0017</h1>
-              </div>
-              <div>
-                <p className="text-sm font-semibold text-gray-500">
-                  CO In-charge
-                </p>
-                <h1 className="text-sm text-neutral-700">James Alagbon</h1>
-              </div>
-              <div>
-                <p className="text-sm font-semibold text-gray-500">Branch</p>
-                <h1 className="text-sm text-neutral-700">Alagbon</h1>
-              </div>
-            </div>
-            <p className="px-5 font-semibold text-neutral-700">Other details</p>
-            <div className="flex gap-6 px-5 py-2">
-              <div>
-                <p className="text-sm font-semibold text-gray-500">
-                  Amount borrowed
-                </p>
-                <h1 className="text-sm text-neutral-700">₦300,000</h1>
-              </div>
-              <div>
-                <p className="text-sm font-semibold text-gray-500">
-                  Date disbursed
-                </p>
-                <h1 className="text-sm text-neutral-700">Dec 30, 2022</h1>
-              </div>
-            </div>
-            <div className="flex px-5 py-2 gap-15">
-              <div>
-                <p className="text-sm font-semibold text-gray-500">
-                  Interest rate
-                </p>
-                <h1 className="text-sm text-neutral-700">5%</h1>
-              </div>
-              <div>
-                <p className="text-sm font-semibold text-gray-500">
-                  Loan Status
-                </p>
-                <div className="flex gap-1 px-2 rounded bg-brand-neutral ">
-                  <img src="/ongoing.svg" alt="ongoing icon" />
-                  <span className="text-sm text-brand-orange">Ongoing</span>
-                </div>
-              </div>
-            </div>
+
+            <LoanSummary
+              isLoading={isLoadingDetails}
+              error={loanDetailsError}
+              loanDetails={loanDetails}
+            />
 
             <p className="px-5 my-4 font-semibold text-md text-neutral-700">
               Repayment History
             </p>
-            <DrawerTable />
+            <LoanRepaymentTable
+              isLoading={isLoadingDetails}
+              error={loanDetailsError}
+              item={loanDetails?.data.repaymentHistory}
+              customerDetails={loanDetails?.data.customerDetails}
+              loanDetails={loanDetails?.data.loanDetails}
+              meta={loanDetails?.meta}
+              onPageChange={(page) =>
+                handlePageChange(page, PaginationKey.loan_page_repayment)
+              }
+            />
           </ul>
         </div>
       </div>
@@ -123,6 +102,7 @@ export default function LoanClient() {
               onPageChange={(page) =>
                 handlePageChange(page, PaginationKey.branch_loan_page)
               }
+              handleClick={handleClick}
             />
           </div>
 
