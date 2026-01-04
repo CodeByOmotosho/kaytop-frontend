16138b6211abf87bab67916455083b852502d106
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-04 06:48:07 +0100

    fix(service): move updateProfile and upload avatar to settingsService

--- app/services/settingsService.ts
+++ app/services/settingsService.ts
@@ -1,18 +1,31 @@
 import apiClient from "@/lib/apiClient";
-import apiServer from "@/lib/apiServer";
 import { apiBaseUrl } from "@/lib/config";
-import { AxiosError } from "axios";
+import { CustomerData } from "../types/customer";
+import { UpdateProfileData, UploadAvatarResponse } from "../types/settings";
 
 export class SettingsService {
-  static async getProfile() {
-    try {
-      const response = await apiServer.get(`${apiBaseUrl}/auth/profileeeee`);
-      console.log(response);
-      return response.data;
-    } catch (error: AxiosError | unknown) {
-      const err = error as AxiosError;
-      console.log("Error fetching profile", err.response?.data);
-      throw err;
-    }
+   static async updateProfile(data: UpdateProfileData): Promise<CustomerData> {
+    const response = await apiClient.patch(`${apiBaseUrl}/users/me`, data);
+    //console.log(response);
+    return response.data;
   }
+
+  static async uploadAvatar(file: File): Promise<UploadAvatarResponse> {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await apiClient.patch(
+      `${apiBaseUrl}/users/me/profile-picture`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data;
+  }
+
+
+ 
 }
