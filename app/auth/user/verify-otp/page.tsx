faa69671c2fc6d4f11473a648bc47b3259b8781d
Faree99 <adebayofaridah99@gmail.com>
2026-01-15 23:08:49 +0100

    updated

--- app/auth/user/verify-otp/page.tsx
+++ app/auth/user/verify-otp/page.tsx
@@ -1,7 +1,5 @@
-"use client"
+import VerifyOtpClient from "./VerifyOtpClient";
 
-import VerifyOtpForm from "@/app/_components/ui/auth/user/VerifyOtpForm";
-import { Purpose } from "@/app/types/auth";
 
 export const metadata = {
   title: "Verify OTP"
@@ -9,17 +7,6 @@ export const metadata = {
 }
 export default function page() {
   
-  return (
-        <div className="bg-white rounded-xl shadow-md p-10 w-full mx-auto max-w-md md:max-w-xl mt-6">
-      <h1 className="text-3xl font-medium text-neutral-700">
-        Verify your email
-      </h1>
-      <p className="text-neutral-700 text-md">
-        Please enter the OTP sent to your email for verification
-      </p>
+       return <VerifyOtpClient />;
 
-      <VerifyOtpForm />
-     
-    </div>
-  );
 }
