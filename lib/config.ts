a54b265007027e4d68b8313268ed06314ca77a62
Code By Omotosho <john.omotosho@yahoo.com>
2026-01-21 15:02:40 +0100

    netlify configuration

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,5 @@
-export const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const apiBaseUrl =
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://kaytop-production.up.railway.app";
 export const OTP_DURATION = 10;
 export const PAGINATION_LIMIT = 10;
